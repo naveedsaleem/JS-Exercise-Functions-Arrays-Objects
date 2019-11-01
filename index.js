eeedf7a6ecc4ff060e05ccabafb92078362bec17
NAVEED SALEEM <naveedsaleem2017@gmail.com>
2019-11-01 19:20:39 -0400

    strech task done

--- index.js
+++ index.js
@@ -265,8 +265,14 @@ const argTimesTwo = (num) => {return num * 2; }; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(odometer) {
+  return {
+    odometer: odometer,
+    drive(distance){ 
+      this.odometer = distance + this.odometer;
+      return this.odometer;
+    }
+  }
 }
 
 /// ////// END OF CHALLENGE /////////
